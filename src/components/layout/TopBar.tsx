de473f9c1206e282d40b134909068110cd85f4ae
Amr Taha <amrshiha2@gmail.com>
2025-03-19 13:53:45 +0200

    feat(internationalization): implemented language switching

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,6 +1,18 @@
-import Image from "next/image";
+"use client";
+
 import { Icons } from "../general/icons";
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useLocale, useTranslations } from "next-intl";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 const TopBar = () => {
   const icons = [
@@ -37,17 +49,10 @@ const TopBar = () => {
   ];
 
   return (
-    <div className="bg-secondary-orange text-white">
+    <div dir="ltr" className="bg-secondary-orange text-white">
       <div className="container mx-auto px-4 flex justify-between items-center py-1.5">
-        <div className="flex items-center">
-          <Image
-            src={"/saudi-arabia.svg"}
-            alt="arabic"
-            width={20}
-            height={20}
-          />
-          <div className="caption-12-medium font-bold">لغة عربية</div>
-        </div>
+        <LanguageSwitcher />
+
         <div className="flex items-center gap-x-4">
           {icons.map((item) => (
             <Link key={item.title} href={item.link}>
@@ -61,3 +66,45 @@ const TopBar = () => {
 };
 
 export default TopBar;
+
+function LanguageSwitcher() {
+  const router = useRouter();
+  const locale = useLocale();
+  const t = useTranslations("language");
+  const language = locale === "en" ? t("en") : t("ar");
+  const iconSrc = locale === "en" ? "/english.svg" : "/arabic.svg";
+
+  const toggleLanguage = (newLocale: "ar" | "en") => {
+    const path = window.location.pathname;
+    const pathWithoutLocale = path.replace(/^\/(en|ar)/, "");
+    router.push(`/${newLocale}${pathWithoutLocale}`);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="flex items-center gap-2 px-2">
+          <div className="flex items-center gap-x-1">
+            <Image src={iconSrc} alt="arabic" width={20} height={20} />
+            <div className="caption-12-medium font-bold">{language}</div>
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => toggleLanguage("ar")}>
+          <div className="flex items-center gap-x-1">
+            <Image src="/arabic.svg" alt="arabic" width={20} height={20} />
+            <div className="caption-12-medium font-bold">{t("ar")}</div>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => toggleLanguage("en")}>
+          <div className="flex items-center gap-x-1">
+            <Image src="/english.svg" alt="english" width={20} height={20} />
+            <div className="caption-12-medium font-bold">{t("en")}</div>
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
