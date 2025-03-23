c25475e4b1e83c4108a5c3a46d2c3844b66e47d2
Amr Taha <amrshiha2@gmail.com>
2025-03-23 10:57:44 +0200

    feat(section): add socialQR section

--- src/components/general/SocialQR.tsx
+++ src/components/general/SocialQR.tsx
@@ -1,7 +1,31 @@
 import Image from "next/image";
-import React from "react";
 
 const SocialQR = () => {
+  return (
+    <section dir="ltr" className="container mx-auto px-4">
+      <div className="relative max-w-3xl mx-auto">
+        <div className="max-w-fit mx-auto p-8 text-center sm:absolute -top-10 right-0">
+          <div className="hidden sm:block w-52 md:w-60 xl:w-full rotate-[-196.26deg] rotate-z-180 rotate-y-180 absolute top-[20%] right-[70%]">
+            <Image src="/arrow.svg" alt="Arrow" width={282.97} height={91.82} />
+          </div>
+          <div className="hidden sm:block w-52 md:w-60 xl:w-full rotate-[-248.91deg] absolute top-[120%] right-[20%] md:top-[130%] md:right-[22%] xl:top-[170%] xl:right-[4%]">
+            <Image src="/arrow.svg" alt="Arrow" width={282.97} height={91.82} />
+          </div>
+
+          <h2 className="text-3xl md:text-4xl lg:text-[3.25rem] font-bold text-primary mb-2">
+            <span className="text">تابعونا</span>
+            <span className="block text-4xl md:text-5xl lg:text-6xl">
+              خطوة بخطوة
+            </span>
+          </h2>
+        </div>
+        <QRSquare />
+      </div>
+    </section>
+  );
+};
+
+const QRSquare = () => {
   const socialQRs = [
     {
       platform: "Snapchat",
@@ -30,53 +54,45 @@ const SocialQR = () => {
   ];
 
   return (
-    <div className="max-w-4xl mx-auto p-8 text-center" dir="rtl">
-      <h2 className="text-4xl font-bold text-primary mb-2">
-        <span>تابعونا</span>
-        <span className="block">خطوة بخطوة</span>
-      </h2>
+    <div className="flex flex-col">
+      <div className="flex justify-start sm:w-fit">
+        <div className="grow sm:shrink">
+          <Image
+            className="w-full sm:w-fit"
+            src={socialQRs[0].image}
+            alt={`${socialQRs[0].platform} QR Code`}
+            width={160}
+            height={160}
+          />
+        </div>
+      </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
-        <div className="flex flex-col items-center">
-          <div className={`relative p-1 rounded-lg mb-2`}>
+      <div className="flex justify-start sm:w-fit">
+        {socialQRs.slice(1, 3).map((item) => (
+          <div key={item.platform} className="grow sm:shrink">
             <Image
-              src={socialQRs[0].image}
-              alt={`${socialQRs[0].platform} QR Code`}
+              className="w-full sm:w-fit"
+              src={item.image}
+              alt={`${item.platform} QR Code`}
               width={160}
               height={160}
             />
           </div>
-        </div>
+        ))}
+      </div>
 
-        <div>
-          {socialQRs.slice(1, 3).map((item) => (
-            <div key={item.platform} className="flex flex-col items-center">
-              <div className={`relative p-1 rounded-lg mb-2`}>
-                <Image
-                  src={item.image}
-                  alt={`${item.platform} QR Code`}
-                  width={160}
-                  height={160}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div>
-          {socialQRs.slice(3).map((item) => (
-            <div key={item.platform} className="flex flex-col items-center">
-              <div className={`relative p-1 rounded-lg mb-2`}>
-                <Image
-                  src={item.image}
-                  alt={`${item.platform} QR Code`}
-                  width={160}
-                  height={160}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
+      <div className="flex justify-start sm:w-fit">
+        {socialQRs.slice(3).map((item) => (
+          <div key={item.platform} className="grow sm:shrink">
+            <Image
+              className="w-full sm:w-fit"
+              src={item.image}
+              alt={`${item.platform} QR Code`}
+              width={160}
+              height={160}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
