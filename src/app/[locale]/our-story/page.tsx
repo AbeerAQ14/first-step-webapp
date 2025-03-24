ded40898c898b15902f5ea31fe60c605617f6a9e
Amr Taha <amrshiha2@gmail.com>
2025-03-24 14:07:53 +0200

    feat(section): add why us section

--- src/app/[locale]/our-story/page.tsx
+++ src/app/[locale]/our-story/page.tsx
@@ -1,6 +1,7 @@
 import Headline from "@/components/general/Headline";
 import VisionMisson from "@/components/general/VisionMission";
 import Values from "@/components/general/Values";
+import WhyUs from "@/components/general/WhyUs";
 
 export default function StoryPage() {
   return (
@@ -8,6 +9,7 @@ export default function StoryPage() {
       <Headline />
       <VisionMisson />
       <Values />
+      <WhyUs />
     </div>
   );
 }
---
+++ src/components/general/WhyUs.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+const WhyUs = () => {
+  const items = [
+    {
+      title: "سهولة البحث والاختيار",
+      description:
+        "نوفر لك أداة بحث متقدمة تتيح تصفية الحضانات حسب الموقع، العمر، مواعيد العمل،‌‌ والخدمات المتاحة، مما يساعدك على العثور على الحضانة المثالية لطفلك بكل سهولة.",
+    },
+    {
+      title: "دعم احتياجات الأطفال المختلفة",
+      description:
+        "نهتم بالأطفال جميعهم، لذا نوفر قسماً خاصاً بمراكز ذوي الاحتياجات الخاصة، مما يسهل‌‌ على الأهالي العثور على المكان الأنسب لرعاية أطفالهم باهتمام واحترافية.",
+    },
+    {
+      title: "التواصل فعال بين العائلات والحضانات",
+      description:
+        "نساعد على تعزيز التواصل بين الأهالي ودور الحضانة من خلال تقارير يومية عن تطور‌‌  الأطفال، مع إمكانية التواصل المباشر مع الإدارة لمتابعة كل جديد.",
+    },
+    {
+      title: "المصداقية والأمان‌",
+      description:
+        "نوفر لك بيانات دقيقة ومحدثة عن الحضانات، مع التزامنا بأعلى معايير الجودة لضمان‌‌ تجربة آمنة وموثوقة للعائلات. نحرص على تيسير عملية البحث واتخاذ القرار بثقة.",
+    },
+  ];
+
+  return (
+    <section className="container mx-auto px-4">
+      <div className="relative flex flex-col md:flex-row items-center justify-between gap-x-4 2xl:gap-x-24">
+        <div className="order-2 md:order-1 md:pl-72 xl:pl-0">
+          <h2 className="text-primary">لماذا First Step؟</h2>
+
+          <div className="mt-9 flex flex-col gap-y-6">
+            {items.map((item) => (
+              <div key={item.title} className="space-y-4">
+                <p className="font-bold text-2xl text-gray">{item.title}</p>
+                <p className="font-medium text-mid-gray">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <Image
+          className="order-1 md:order-2 grow md:absolute inset-y-0 -left-40 -z-50 xl:relative xl:left-0 shrink-0"
+          src="/mother-and-daughter.png"
+          alt="Mother And Daughter"
+          width={508}
+          height={499.33}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default WhyUs;
