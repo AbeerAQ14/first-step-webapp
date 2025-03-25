b3dadaec78874b76fec78252972cf8e19b9699c2
Amr Taha <amrshiha2@gmail.com>
2025-03-26 00:10:34 +0200

    feat(page): add home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,21 @@
+import Advertisment from "@/components/general/Advertisment";
+import Headline from "@/components/general/Headline";
+import VisionMission from "@/components/general/VisionMission";
+import Values from "@/components/general/Values";
+import Blogs from "@/components/general/blog/Blogs";
 import FAQs from "@/components/general/FAQs";
+import Contact from "@/components/general/contact/Contact";
 
 export default function HomePage() {
   return (
-    <div>
+    <main>
+      <Advertisment />
+      <Headline />
+      <VisionMission />
+      <Values />
+      <Blogs />
       <FAQs />
-    </div>
+      <Contact />
+    </main>
   );
 }
