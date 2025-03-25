698468cfa4dc88ec6470310edc1514fa43f21f7f
Amr Taha <amrshiha2@gmail.com>
2025-03-25 11:21:37 +0200

    feat(section): add services section

---
+++ src/app/[locale]/services/page.tsx
@@ -0,0 +1,11 @@
+import Headline from "@/components/general/Headline";
+import Services from "@/components/general/Services";
+
+export default function ServicesPage() {
+  return (
+    <main>
+      <Headline />
+      <Services />
+    </main>
+  );
+}
