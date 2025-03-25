04d1d1942ac69b1a3a115231cd4fdf5c05f3f1c5
Amr Taha <amrshiha2@gmail.com>
2025-03-25 13:43:05 +0200

    feat(section): add advertisement section

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -1,9 +1,11 @@
+import Advertisment from "@/components/general/Advertisment";
 import Headline from "@/components/general/Headline";
 import Services from "@/components/general/Services";
 
 export default function ServicesPage() {
   return (
     <main>
+      <Advertisment />
       <Headline />
       <Services />
     </main>
