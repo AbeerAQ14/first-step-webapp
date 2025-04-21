ebe81ccf9f970ab08df57705c5025906572a186d
Amr Taha <amrshiha2@gmail.com>
2025-04-21 17:59:51 +0200

    feat(nurseries): add branches section

--- src/components/general/nurseries/Branches.tsx
+++ src/components/general/nurseries/Branches.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { Icons } from "../icons";
 
 const Branches = () => {
@@ -14,16 +13,18 @@ const Branches = () => {
 
   return (
     <section className="container mx-auto px-4 xl:px-8">
-      <div className="">
-        <h2 className="heading-3 text-secondary-burgundy">فروع الحضانة</h2>
+      <h2 className="mb-6 heading-3 text-secondary-burgundy text-center">
+        فروع الحضانة
+      </h2>
 
-        <div className="relative flex flex-wrap justify-center">
+      <div className="relative overflow-x-auto overflow-y-hidden px-4">
+        <div className="flex flex-nowrap pb-4 min-h-[120px]">
           {branches.map((branch, index) => (
             <div
               key={branch}
-              className={`group relative flex flex-col items-center w-28 md:w-48 mb-8 ${branchColors[index]}`}
+              className={`group relative flex flex-col items-center min-w-48 md:min-w-64 w-48 mb-8 ${branchColors[index]}`}
             >
-              <div className="-z-50 w-full h-1 bg-light-gray absolute translate-y-[670%] top-1/2" />
+              <div className="-z-50 w-full h-1 bg-light-gray absolute translate-y-[670%] top-1/2 group-first:w-1/2 group-last:w-1/2 group-first:right-0 group-last:left-0 rtl:group-last:right-0 rtl:group-first:right-auto rtl:group-first:left-0" />
 
               {/* Branch circle with color based on index */}
               <div
@@ -35,7 +36,7 @@ const Branches = () => {
               </div>
 
               {/* Branch name */}
-              <p className="absolute left-1/2 -translate-x-1/2 group-odd:top-[20%] group-even:top-[100%] text-2xl text-center font-bold text-nowrap">
+              <p className="absolute left-1/2 -translate-x-1/2 group-odd:top-[20%] group-even:top-[100%] text-2xl text-center font-bold text-nowrap whitespace-nowrap">
                 {branch}
               </p>
             </div>
