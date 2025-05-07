c9347379c37f3a42676e19c431c865efabd8244f
Amr Taha <amrshiha2@gmail.com>
2025-05-07 11:25:37 +0300

    update image links

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "http",
-        hostname: "1a39-196-128-86-90.ngrok-free.app",
+        hostname: "c4e8-196-128-75-129.ngrok-free.app",
         port: "",
       },
     ],
