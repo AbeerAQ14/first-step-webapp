859e0ffca9afd26a414354ae2f7373e7d096d3d7
Amr Taha <amrshiha2@gmail.com>
2025-05-13 14:57:48 +0300

    fix: fix use client issue

--- src/components/forms/dashboard/adblog-request/CenterAdRequest.tsx
+++ src/components/forms/dashboard/adblog-request/CenterAdRequest.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import AdRequestForm from "./AdRequestForm";
 import { useRouter } from "@/i18n/navigation";
@@ -8,19 +10,21 @@ const CenterAdRequest = () => {
   return (
     <div>
       <AdRequestForm>
-        <>
-          <Button size={"sm"} type="submit">
-            إرسال طلب
-          </Button>
-          <Button
-            size={"sm"}
-            variant={"outline"}
-            className="!border-light-gray text-mid-gray"
-            onClick={() => router.back()}
-          >
-            إلغاء
-          </Button>
-        </>
+        {(data, isValid) => (
+          <>
+            <Button size={"sm"} type="submit">
+              إرسال طلب
+            </Button>
+            <Button
+              size={"sm"}
+              variant={"outline"}
+              className="!border-light-gray text-mid-gray"
+              onClick={() => router.back()}
+            >
+              إلغاء
+            </Button>
+          </>
+        )}
       </AdRequestForm>
     </div>
   );
