26a93189d041a84e627901cfe045e1b366d88b52
Amr Taha <amrshiha2@gmail.com>
2025-05-14 13:41:39 +0300

    fix: update parent reports page heading

--- src/app/[locale]/dashboard/parent/daily-reports/page.tsx
+++ src/app/[locale]/dashboard/parent/daily-reports/page.tsx
@@ -127,7 +127,7 @@ const parentsData: Report[] = [
 export default function DailyReports() {
   return (
     <div className="lg:p-4 space-y-1">
-      <p className="heading-4 text-primary text-center">أولياء الأمور</p>
+      <p className="heading-4 text-primary text-center">التقارير اليومية</p>
 
       <DataTable columns={columns} data={parentsData} />
     </div>
