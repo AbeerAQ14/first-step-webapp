daaf9b2fd0784c0fadeb92a8bb6848369b0f6da2
Amr Taha <amrshiha2@gmail.com>
2025-05-14 18:29:59 +0300

    feat(component): update AdDetailsWrapper w/ adType and handlers

--- src/components/dashboard/advertisement/AdDetailsWrapper.tsx
+++ src/components/dashboard/advertisement/AdDetailsWrapper.tsx
@@ -5,33 +5,108 @@ import { Button } from "@/components/ui/button";
 import AdRequestForm from "@/components/forms/dashboard/adblog-request/AdRequestForm";
 import { AdRequestFormData } from "@/lib/schemas";
 
+type AdType = "accepted" | "pending" | "rejected";
+
+// just show and edit
 const AdDetailsWrapper = ({
   adId,
   initialValues,
   mode,
+  adType,
 }: {
   adId: string;
   initialValues: AdRequestFormData;
   mode: "add" | "show";
+  adType?: AdType;
 }) => {
+  const editHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Edited Ad");
+  };
+
+  const rejectHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Rejected Ad");
+  };
+
+  const acceptHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Accepted Ad");
+  };
+
+  const deleteHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Deleted Ad");
+  };
+
+  const adTypeButtons = (
+    adType: AdType,
+    data: AdRequestFormData,
+    isValid: boolean
+  ) => {
+    switch (adType) {
+      case "accepted":
+        return (
+          <>
+            <Button asChild size={"sm"}>
+              <Link href={`${adId}/edit`}>تعديل الإعلان</Link>
+            </Button>
+            <Button
+              onClick={deleteHandler}
+              size={"sm"}
+              variant={"outline"}
+              className="!border-destructive text-destructive"
+            >
+              حذف الإعلان
+            </Button>
+          </>
+        );
+      case "pending":
+        return (
+          <>
+            <Button onClick={acceptHandler} size={"sm"}>
+              قبول الإعلان
+            </Button>
+            <Button
+              onClick={rejectHandler}
+              size={"sm"}
+              variant={"outline"}
+              className="!border-destructive text-destructive"
+            >
+              رفض الإعلان
+            </Button>
+          </>
+        );
+      case "rejected":
+        return (
+          <>
+            <Button onClick={acceptHandler} size={"sm"}>
+              قبول الإعلان
+            </Button>
+            <Button
+              asChild
+              size={"sm"}
+              variant={"outline"}
+              className="!border-light-gray text-mid-gray"
+            >
+              <Link href={`${adId}/edit`}>تعديل الإعلان</Link>
+            </Button>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
   const buttons = (data: AdRequestFormData, isValid: boolean) => {
     if (mode === "add") {
       return (
         <>
-          <Button
-            onClick={(e) => {
-              e.preventDefault();
-              console.log(data, isValid);
-            }}
-            size={"sm"}
-          >
+          <Button onClick={editHandler} size={"sm"}>
             تعديل الإعلان
           </Button>
           <Button
-            onClick={(e) => {
-              e.preventDefault();
-              console.log(data, isValid);
-            }}
+            onClick={deleteHandler}
             size={"sm"}
             variant={"outline"}
             className="!border-destructive text-destructive"
@@ -41,24 +116,7 @@ const AdDetailsWrapper = ({
         </>
       );
     } else if (mode === "show") {
-      return (
-        <>
-          <Button asChild size={"sm"}>
-            <Link href={`${adId}/edit`}>تعديل الإعلان</Link>
-          </Button>
-          <Button
-            onClick={(e) => {
-              e.preventDefault();
-              console.log(data, isValid);
-            }}
-            size={"sm"}
-            variant={"outline"}
-            className="!border-destructive text-destructive"
-          >
-            حذف الإعلان
-          </Button>
-        </>
-      );
+      return adTypeButtons(adType || "accepted", data, isValid);
     }
   };
 
