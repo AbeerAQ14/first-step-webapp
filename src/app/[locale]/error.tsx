4d763d4da701109ac7a506b9fb8afde8258b017e
Amr Taha <amrshiha2@gmail.com>
2025-05-24 15:46:09 +0300

    feat(i18n): enhance error and not-found pages with localized messages

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import { useEffect } from "react";
 import { AlertTriangle } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function Error({ error }: { error: Error }) {
+  const t = useTranslations("error");
+
   useEffect(() => {
     console.error("App error:", error);
   }, [error]);
@@ -17,24 +20,24 @@ export default function Error({ error }: { error: Error }) {
           <AlertTriangle className="w-16 h-16" />
         </div>
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">
-          Oops! Something went wrong.
+          {t("title")}
         </h1>
         <p className="text-lg text-gray-600 dark:text-gray-300 mb-2">
-          {error?.message || "We couldn't process your request."}
+          {error?.message || t("default-message")}
         </p>
         <p className="text-sm text-gray-500 dark:text-gray-400 mb-6">
-          Please try refreshing the page or return to the home screen.
+          {t("description")}
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4">
           <Button size={"sm"} onClick={() => location.reload()}>
-            Refresh Page
+            {t("refresh")}
           </Button>
           <Button asChild variant={"ghost"} size={"sm"}>
             <Link
               href="/"
               className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 transition"
             >
-              Go to Homepage
+              {t("home")}
             </Link>
           </Button>
         </div>
