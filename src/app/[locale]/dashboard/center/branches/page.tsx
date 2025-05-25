6eb0afd283542122b9e4426cc3e2b888434aec1d
Amr Taha <amrshiha2@gmail.com>
2025-05-25 15:24:16 +0300

    fix: fix page minor issue

--- src/app/[locale]/dashboard/center/branches/page.tsx
+++ src/app/[locale]/dashboard/center/branches/page.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import Branches from "@/components/dashboard/branches/Branches";
 
-export default async function CenterDashboardHome() {
+export default function CenterDashboardHome() {
   const t = useTranslations("dashboard.center.branches");
 
   return (
