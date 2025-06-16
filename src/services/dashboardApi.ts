beb00b989f559f730f59d36d522ec89720949f58
Amr Taha <amrshiha2@gmail.com>
2025-06-16 14:35:18 +0300

    feat(advertisement): retreive center ads from api

--- src/services/dashboardApi.ts
+++ src/services/dashboardApi.ts
@@ -758,7 +758,7 @@ export const adminService = {
   getAllCenterAds: async () => {
     try {
       const response = await apiClient.get(`/dashboard/all-centers-ads`);
-      return response.data;
+      return response.data.data;
     } catch (error) {
       throw ApiErrorHandler.handle(error);
     }
